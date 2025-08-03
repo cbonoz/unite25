7d7b637623bc58ecc9c07e8196b6f3a76dadad0b
Chris Buonocore <chrisdistrict@gmail.com>
2025-08-03 11:41:34 -0400

    update title

--- app/siteConfig/index.tsx
+++ app/siteConfig/index.tsx
@@ -1,7 +1,7 @@
 
 
 export const siteConfig = {
-  name: "SwapJar | Multichain donation link-in-bios",
+  name: "SwapJar | Multichain payment links",
   description: "Create your own payment jar to receive payments in any token, automatically converting to your preferred stablecoin.",
   appUrl: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
   youtubeUrl: "https://youtu.be/RNcDxn0VMPw",
